a99bd027f3b267dc5b3bd986418e91cdb8468e0c
adapateja <126445884+adapateja@users.noreply.github.com>
2024-02-21 18:21:37 +0530

    Add files via upload

---
+++ db.config.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+
+const baseUrl = process.env.MONGODB || "0.0.0.0:27017";
+const url = process.env.DB_URL;
+
+export const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(`mongodb://${baseUrl}/chattingApplication`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    console.log("MongoDB connected using mongoose");
+  } catch (err) {
+    console.log(`Error connecting to the database: `, err);
+  }
+};
